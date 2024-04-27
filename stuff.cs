a9aebeb3d6f71f76b46829835e138038801d7365
Christopher Hylton <jamboyh5@gmail.com>
2024-04-27 16:03:04 -0600

    grade calculator stuffs

--- stuff.cs
+++ stuff.cs
@@ -1,3 +1,22 @@
-string UserInput;
-ConsoleWrite("prompt")
-UserInput=ConsoleReadline();
+x = 5
+y = 4
+z = 4
+
+
+
+
+
+
+
+if (x > y) 
+{
+    Console. WriteLine("Greater Than");
+}
+
+if (x > y) 
+{
+    if (x > y)
+    { 
+        Console.WriteLine("greater than both.");
+    }
+}
