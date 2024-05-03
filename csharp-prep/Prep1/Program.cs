41f9150ef3525b44eed2f43ca4c43d9dc7da21c3
Christopher Hylton <jamboyh5@gmail.com>
2024-05-03 13:45:23 -0600

    more guess number stuffs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,19 +1,41 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        string userinput = Console.ReadLine();
-        int number = int.Parse(userinput);
+        do 
+        {
 
-        Console.Write("What is your guess? ");
-        
-    }
+            Console.Write("What is the magic number? ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            int numberGuess = int.Parse(userGuess);
+
+            if (numberGuess < number)
+            {
+                Console.WriteLine("Try a higher number. ");
+
+
+            }
+            if (numberGuess > number)
+            {
+                Console.WriteLine("Try a lower number. ");
+            }
+
+            else 
+            {
+                Console.Write("You guessed it! Congratulations! ");
+            };
+        } while (numberGuess != number);
+
 
     private static string ConsoleReadLine()
     {
         throw new NotImplementedException();
     }
-}
+}}
