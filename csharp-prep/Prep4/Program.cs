6dc320d4910bfa6967ec44f2997e3208859111de
Christopher Hylton <jamboyh5@gmail.com>
2024-05-03 23:03:15 -0600

    we have to ask the instructor about this

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,12 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Please enter a list of numbers, enter 0 when finished. ");
+        Console.WriteLine(" Please enter a list of numbers, enter 0 when finished. ");
         
+        // Makes an empty list.
         List<int> numbers = [];
+        int number;
 
-        Console.WriteLine("Enter number. ");
+        do 
+        {
+        //The program asks for numbers to make up the list.  
+        Console.Write(" Enter number. ");
         string userInput = Console.ReadLine();
+        try
+        {
+        number = int.Parse(userInput); 
+        numbers.Add(number);
+        if (number == 0);
+        } catch (FormatException)
+        {
+            Console.WriteLine("I'm sorry, I don't recognize that. Please try again. ");
+        }
+        
+        {
+        Console.WriteLine($"Your list is: {string.Join(", ", numbers)}");        }
+
+        } while (number != 0);
+
+
         
 
     }
