1c5585fe0ca84ec2360eafa0d5e142fc40da3217
Christopher Hylton <jamboyh5@gmail.com>
2024-05-03 18:17:42 -0600

    prep 3 change

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int numberGuess;
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+
+        // Console.Write("What is the magic number? ");
+        // string userInput = Console.ReadLine();
+        // int number = int.Parse(userInput);
+        do 
+        {
+
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            numberGuess = int.Parse(userGuess);
+
+            if (numberGuess < number)
+            {
+                Console.WriteLine("Try a higher number. ");
+
+
+            }
+            else if (numberGuess > number)
+            {
+                Console.WriteLine("Try a lower number. ");
+            }
+
+            else 
+            {
+                Console.Write("You guessed it! Congratulations! ");
+            }
+        } while (numberGuess != number);
     }
 }
