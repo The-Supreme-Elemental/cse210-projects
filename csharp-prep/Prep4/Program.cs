d12efdab839fcca8256adc6e458bc3022ed0b1ad
Christopher Hylton <jamboyh5@gmail.com>
2024-05-03 18:38:15 -0600

    prep $4 list start

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.Write("Please enter a list of numbers, enter 0 when finished. ");
+        
+        List<int> numbers = [];
+
+        Console.WriteLine("Enter number. ");
+        string userInput = Console.ReadLine();
+        
+
     }
 }
