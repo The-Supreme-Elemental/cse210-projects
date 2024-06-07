877b61b9a2aa5bb5a3daac53f204fb63756a1f9f
Christopher Hylton <jamboyh5@gmail.com>
2024-06-07 16:58:34 -0600

    Inheritance Prepare Activity

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    
+
+    public MathAssignment(string name, string topic, string section, string problems) : base(name, topic)
+    {
+        _textbookSection = section;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        
+        return $"{GetSummary()}\nSection {_textbookSection}, Problems {_problems}";
+    }
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
+    
+    
+    
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new("Samuel Bennet", "Multiplication");
+        MathAssignment mathAssignment = new("Mark Anthony", "Calculus", "13.5", "45-87, odd");
+        WritingAssignment writingAssignment = new("Mary Waters", "European History", "The Causes of World War II");
+
+        // string summary = myAssignment.GetSummary();
+        // string summary = mathAssignment.GetHomeworkList();
+        string summary = writingAssignment.GetWritingInformation();
+        Console.WriteLine(summary);
     }
+
+
+
 }
