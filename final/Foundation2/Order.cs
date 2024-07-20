9ef0bdf82f8f75d4594808d84f5b3a3e2bf238cd
Christopher Hylton <jamboyh5@gmail.com>
2024-07-20 12:56:31 -0600

    foundation 2

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,6 @@
+public class Order
+{
+    private List<Product> Products = new List<Product>();
+
+    private Customer _customer;
+}
---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,37 @@
+public class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _stateOrProvince;
+    private string _country;
+    private int _zipCode;
+
+    public Address(string streetAddress, string city, string stateOrProvince, string country, int zipCode)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _stateOrProvince = stateOrProvince;
+        _country = country;
+        _zipCode = zipCode;
+    }
+
+    public bool IsDomesticUSA()
+    {
+        bool IsDomestic = false;
+
+        Console.Write("Is this package going in or outside of the US? (inside/outside)");
+        string userInput = Console.ReadLine();
+        if (userInput == "inside")
+        {
+            IsDomestic = true;
+        } else {
+            IsDomestic = false;
+        }
+        return IsDomestic;
+    }
+
+    public string GetAddressLabel()
+    {
+        return $"{_streetAddress}\n{_city}, {_stateOrProvince} {_zipCode}\n{_country.ToUpper()}";
+    }
+}
